64f3df1183c8dc90844ac3926b9c5cd8c4986d3e
aasrir <asrir.anass@gmail.com>
2020-05-21 17:55:38 +0200

    added configuration for google chrome

--- src/test/java/utils/BrowserFactory.java
+++ src/test/java/utils/BrowserFactory.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.HashMap;
 
@@ -9,12 +10,19 @@ public class BrowserFactory {
     static WebDriver factoryDriver;
     private static HashMap<String, String> configurationMap = PropertiesFile.read("src/test/resources/environnement/config.properties");
     static String webBrowserType = configurationMap.get("browser");
+    static boolean headless = Boolean.parseBoolean(configurationMap.get("isheadless"));
 
     public static WebDriver getFactoryDriver() {
         switch (webBrowserType) {
             case "chrome":
                 System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-                factoryDriver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--start-maximized");
+                options.addArguments("--ignore-certificate-errors");
+                options.addArguments("--disable-popup-blocking");
+                options.addArguments("--incognito");
+                options.setHeadless(headless);
+                factoryDriver = new ChromeDriver(options);
                 break;
             case "ie":
                 break;
