4f2b103737f0aef229dd417f2c44cdd57893bafd
aasrir <asrir.anass@gmail.com>
2020-05-22 17:46:34 +0200

    added configuration for edge-legacy ~instable

--- src/test/java/utils/BrowserFactory.java
+++ src/test/java/utils/BrowserFactory.java
@@ -3,6 +3,7 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
@@ -35,6 +36,8 @@ public class BrowserFactory {
                 System.setProperty("webdriver.gecko.driver", driversPath + "geckodriver.exe");
                 factoryDriver = new FirefoxDriver();
                 break;
+            case "edge-legacy":
+                factoryDriver = new EdgeDriver();
             default:
                 break;
         }
