c2e72ee93e2cff733f9246ee75645bc5b3aa41aa
Anass <anass.asrir@enablon.com>
2021-04-30 11:56:27 +0200

    implemented keyword driven model

--- src/test/java/step_definition/ui/GoogleSearchStepDefinition.java
+++ src/test/java/step_definition/ui/GoogleSearchStepDefinition.java
@@ -3,8 +3,14 @@ package step_definition.ui;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import utils.ExcelReader;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import static modules.Hooks.driver;
 import static org.junit.Assert.assertEquals;
@@ -12,6 +18,9 @@ import static org.junit.Assert.assertTrue;
 import static modules.page_objects.GoogleSearchPage.searchFor;
 
 public class GoogleSearchStepDefinition {
+    ExcelReader reader = new ExcelReader();
+    String excelFilePath = "src/test/resources/Data/ExcelPOC.xlsx";
+    String lookupWord, expectedResult;
 
     @Given("I open google search page")
     public void iOpenGoogleSearchPage() {
@@ -33,5 +42,23 @@ public class GoogleSearchStepDefinition {
     @Then("search results display the word {string}")
     public void searchResultsDisplayTheWord(String arg0) {
         assertEquals(arg0 + " - Recherche Google", driver.getTitle());
+    }
+
+    @Given("my data is stored in sheet {string} and row {int}")
+    public void myDataIsStoredInSheetAndRowRow_number(String sheetName, Integer rowNumber) throws IOException, InvalidFormatException {
+        List<Map<String,String>> testData =
+                reader.getData(excelFilePath, sheetName);
+        lookupWord = testData.get(rowNumber).get("lookup_word");
+        expectedResult = testData.get(rowNumber).get("expected_result");
+    }
+
+    @When("I lookup the word")
+    public void iLookupTheWord() {
+        searchFor(lookupWord);
+    }
+
+    @Then("Then search should display the expected results")
+    public void thenSearchShouldDisplayTheExpectedResults() {
+        assertEquals(expectedResult + " - Recherche Google", driver.getTitle());
     }
 }
--- target/test-classes/features/ui/GoogleSearch.feature
+++ target/test-classes/features/ui/GoogleSearch.feature
@@ -6,19 +6,26 @@ Feature: Google Search
     Given I open google search page
 
   @smoke
-  Scenario: selenium
+  Scenario: Basic scenario
     When I lookup the word "selenium"
     Then search results display the word "selenium"
 
-  Scenario: cucumber
-    When I lookup the word "cucumber"
-    Then search results display the word "cucumber"
-
   @ignore
-  Scenario Template: <lookup_word>
+  Scenario Template: <title>
     When I lookup the word "<lookup_word>"
     Then search results display the word "<expected_result>"
     Examples:
-      | lookup_word | expected_result |
-      | restassured | restassured     |
-      | java        | fail            |
+      | title                                   | lookup_word | expected_result |
+      | Passing scenario using Gherkin keywords | dog         | dog             |
+      | Failing scenario using Gherkin keywords | cat         | dog             |
+
+  @keyword
+  Scenario Outline: <title>
+    Given my data is stored in sheet "<sheet_name>" and row <row_number>
+    When I lookup the word
+    Then Then search should display the expected results
+
+    Examples:
+      | title                                 | sheet_name | row_number |
+      | Passing scenario using Excel keywords | Sheet1     | 0          |
+      | Failing scenario using Excel keywords | Sheet1     | 1          |
